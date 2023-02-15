af792e0be0c5c0d53f2faf588367c2e6a6227b34
Juliana Novais <juliana.novais@icloud.com>
2023-02-14 22:55:36 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      {" "}
+      <h1>Olá Mundo!</h1>
+      <p>Gbrielle Araújo</p>
+      <p>Juliana Novais</p>
+      <p>Laiane Barbalho</p>
+      <p>Washington Luis</p>
+    </div>
+  );
+}
+
+export default App;
